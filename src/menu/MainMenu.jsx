0a381fb3e69323b36f14ec74ad707149c886d707
Wilde <gutopicciani@yahoo.com.br>
2018-03-30 03:12:24 -0300

    Ajustando cores do botao para tema verde

--- src/menu/MainMenu.jsx
+++ src/menu/MainMenu.jsx
@@ -1,40 +1,54 @@
 import React from 'react';
-import Button from 'material-ui/Button';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles'
 import { Link } from 'react-router-dom';
+import Button from 'material-ui/Button';
+import { withStyles } from 'material-ui/styles'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import logo from '../img/Screen.png';
 import './MainMenu.css';
 
+const styles = theme => ({
+  root: {
+    padding: (theme.spacing.unit * 3),
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+  },
+  img: {
+    maxWidth: '150px',
+    margin: '0 auto',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+});
+
+const greenTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#40B540',
+      dark: '#30A14C',
+      contrastText: '#FFFFFF',
+    },
+  },
+});
+
 function MainMenu(props) {
   const { classes } = props;
-  const buttonClasses = classes.button + ' main-menu-button';
-  const greenButtonClasses = classes.button + ' main-menu-button green-button';
   return (
-    <div className="main-menu-container">
+    <div className={classes.root}>
       <img className={classes.img} src={logo} alt="Logo" />
-      <Link to="/app/principal/777">
-        <Button variant="raised" color="primary" className={buttonClasses}>Cidade Canção</Button>
+      <Link to="/app/principal/777" className={classes.link}>
+        <Button variant="raised" color="primary" className={classes.button} fullWidth>Cidade Canção</Button>
       </Link>
-      <Link to="/app/principal/778">
-        <Button variant="raised" color="primary" className={greenButtonClasses}>Cidade Verde</Button>
+      <Link to="/app/principal/778" className={classes.link}>
+        <MuiThemeProvider theme={greenTheme}>
+          <Button variant="raised" color="primary" className={classes.button} fullWidth>Cidade Verde</Button>
+        </MuiThemeProvider>
       </Link>
     </div>
   );
 }
-
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit
-  },
-  input: {
-    display: 'none',
-  },
-  img: {
-    maxWidth: '150px',
-    margin: '0 auto'
-  },
-});
 
 MainMenu.propTypes = {
   classes: PropTypes.object.isRequired,
